a9b3aaee8bc80fc349b6c39bd1e238b81dd3fd7c
MaiDTTM <68932625+MaiDao99@users.noreply.github.com>
2021-08-21 17:06:32 +0700

    MongLV: Chỉnh lại file server.js để gọn code hơn

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ const { parse } = require('url');
 const next = require('next');
 
 // const dev = process.env.NODE_ENV !== 'production';
-const dev = 'producttion' !== 'production';
 const port = process.env.PORT || 4040;
-const app = next({ dev });
+const app = next({});
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -23,15 +22,8 @@ app.prepare().then(() => {
         // Be sure to pass `true` as the second argument to `url.parse`.
         // This tells it to parse the query portion of the URL.
         const parsedUrl = parse(req.url, true);
-        const { pathname, query } = parsedUrl;
-
-        if (pathname === '/a') {
-            app.render(req, res, '/a', query);
-        } else if (pathname === '/b') {
-            app.render(req, res, '/b', query);
-        } else {
-            handle(req, res, parsedUrl);
-        }
+        // const { pathname, query } = parsedUrl;
+        handle(req, res, parsedUrl);
     }).listen(port, (err) => {
         if (err) throw err;
         console.log('> Ready on http://localhost:' + port);
